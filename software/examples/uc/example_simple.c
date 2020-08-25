f95c5c351247894eba9a56da30a901b747f65493
Erik Fleckstein <erik@tinkerforge.com>
2020-08-25 15:54:31 +0200

    uc: Fix whitespace

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,14 +1,9 @@
-
-
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_rgb_led_v2.h"
 
 #define UID "XYZ" // Change XYZ to the UID of your RGB LED Bricklet 2.0
 
 void check(int rc, const char* msg);
-
-
-
 
 TF_RGBLEDV2 rl;
 
