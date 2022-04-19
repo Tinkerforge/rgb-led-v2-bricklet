f0664e83a98d08c40e7f708ad2d4392005d014b6
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 16:04:17 +0200

    examples: uc: Rename TF_HalContext to TF_HAL.

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -9,13 +9,13 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 static TF_RGBLEDV2 rl;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_rgb_led_v2_create(&rl, UID, hal), "create device object");
 
@@ -23,7 +23,7 @@ void example_setup(TF_HalContext *hal) {
 	check(tf_rgb_led_v2_set_rgb_value(&rl, 0, 170, 234), "call set_rgb_value");
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
